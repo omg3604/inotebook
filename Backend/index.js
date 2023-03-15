7dddeb38c798bfd30d92d41e8fdceb6f85cce45a
Om Golhani <omgolhani3604@gmail.com>
2023-03-16 01:22:43 +0530

    connected CRUD with server

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,13 @@
 const connectToMongo = require('./db');
 const express = require('express')
+var cors = require('cors');
 
 connectToMongo();       // function for establishing connectivity to MongoDB
 const app = express() // express object
 const port = 5000
 
 // middleware for using the body of the request
+app.use(cors());
 app.use(express.json());
 
 // Available routes
